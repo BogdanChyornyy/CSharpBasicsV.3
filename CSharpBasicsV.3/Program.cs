d09e2e4c4f359c66d647221eac9aa7b1fcf5bb3a
BogdanChyornyy <booggiman@mail.ru>
2022-04-09 14:05:02 +0300

    Выполнил домашнее задание к уроку 4, проект №1

--- CSharpBasicsV.3/Program.cs
+++ CSharpBasicsV.3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Homework_4_part_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string FLP = GetFullName();
+            Print(FLP);
+        }
+        static string GetFullName()
+        {
+            Print("Hi! Enter your personal data, please:)");
+            Print("Enter u're first name:");
+            string firstName = ReadString();
+            Print("Enter u're last name:");
+            string lastName = ReadString();
+            Print("Enter u're patronymic:");
+            string patronymic = ReadString();
+            Print("Thx:) You've got pretty name! Youre full name is: ");
+            return lastName + " " + firstName + " " + patronymic;
+        }
+        static string Enter()
+        {
+            return Console.ReadLine();
+        }
+        static void Print(string words)
+        {
+            Console.WriteLine(words);
+        }
+        static string ReadString()
+        {
+            return Convert.ToString(Enter());
+        }
+    }
+}
