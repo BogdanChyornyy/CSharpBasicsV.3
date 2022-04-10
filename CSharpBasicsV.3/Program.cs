f7efcda00b004da653f9739ab6110267c3154b4c
BogdanChyornyy <booggiman@mail.ru>
2022-04-10 16:44:52 +0300

    Выполнил домашнее задание к уроку 4, проект №3.

--- CSharpBasicsV.3/Program.cs
+++ CSharpBasicsV.3/Program.cs
@@ -7,27 +7,80 @@ namespace Homework_4_part_1
 {
     class Program
     {
-        //        Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и
-        //возвращающую число — сумму всех чисел в строке.Ввести данные с клавиатуры и вывести
-        //результат на экран.
-        static void Main(string[] args)
+        enum Season
         {
-            int ansver = Split();
-            Console.WriteLine(ansver);
+            winter,
+            spring,
+            summer,
+            autumn
         }
 
-        static int Split()
+        static void Main()
         {
-            string SomeString = Console.ReadLine();
-            string[] SplitString = SomeString.Split(' ');
+            object r = SeasonDefinition();
+            Console.WriteLine("Your chosen season is: " + r);            
+        }
 
-            int el = 0;
-            for (int i = 0; i < SplitString.Length; i++)
+        static object SeasonDefinition()
+        {
+            string seasName = Convert.ToString(SerNumToSeason());
+            if (seasName == "winter")
             {
-                int m = Convert.ToInt32(SplitString[i]);
-                el += m;
+                seasName = "Зима";
             }
-            return el;
+            else if (seasName == "spring")
+            {
+                seasName = "Весна";
+            }
+            else if (seasName == "summer")
+            {
+                seasName = "Лето";
+            }
+            else
+            {
+                seasName = "Осень";
+            }
+
+            return seasName;
+        }
+
+        static object SerNumToSeason()
+        {            
+            int serNum = Convert.ToInt16(CorrectInt());
+            if (serNum > 0 && serNum < 3 || serNum == 12)
+            {
+                serNum = 0; //winter
+            }
+            else if (serNum > 2 && serNum < 6)
+            {
+                serNum = 1; //spring
+            }
+            else if (serNum > 5 && serNum < 9)
+            {
+                serNum = 2; //summer
+            }
+            else
+            {
+                serNum = 3; //autumn
+            }
+
+            Type enumType = Season.spring.GetType();
+            int example = serNum;
+            return Enum.ToObject(enumType, example);            
+        }
+
+        static int CorrectInt()
+        {
+            Console.WriteLine("Enter serial number of month, from season u've chosen: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            while (num < 1 || num > 12)
+            {
+                Console.WriteLine("Your number is not correct. You should to chose number from 1 to 12. Try again.");
+                num = int.Parse(Console.ReadLine());
+            }
+            return num;
         }
     }
 }
