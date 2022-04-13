ccc7095260536328c05a40ffbcd93266f2ced5f9
BogdanChyornyy <booggiman@mail.ru>
2022-04-14 01:51:01 +0300

    Выполнил домашнее задание к уроку 5, проект №1.

--- CSharpBasicsV.3/Program.cs
+++ CSharpBasicsV.3/Program.cs
@@ -3,84 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace Homework_4_part_1
+using System.IO;
+namespace HelloWorld
 {
     class Program
     {
-        enum Season
+        static void Main(string[] args)
         {
-            winter,
-            spring,
-            summer,
-            autumn
-        }
-
-        static void Main()
-        {
-            object r = SeasonDefinition();
-            Console.WriteLine("Your chosen season is: " + r);            
-        }
-
-        static object SeasonDefinition()
-        {
-            string seasName = Convert.ToString(SerNumToSeason());
-            if (seasName == "winter")
-            {
-                seasName = "Зима";
-            }
-            else if (seasName == "spring")
-            {
-                seasName = "Весна";
-            }
-            else if (seasName == "summer")
-            {
-                seasName = "Лето";
-            }
-            else
-            {
-                seasName = "Осень";
-            }
-
-            return seasName;
-        }
-
-        static object SerNumToSeason()
-        {            
-            int serNum = Convert.ToInt16(CorrectInt());
-            if (serNum > 0 && serNum < 3 || serNum == 12)
-            {
-                serNum = 0; //winter
-            }
-            else if (serNum > 2 && serNum < 6)
-            {
-                serNum = 1; //spring
-            }
-            else if (serNum > 5 && serNum < 9)
-            {
-                serNum = 2; //summer
-            }
-            else
-            {
-                serNum = 3; //autumn
-            }
-
-            Type enumType = Season.spring.GetType();
-            int example = serNum;
-            return Enum.ToObject(enumType, example);            
-        }
-
-        static int CorrectInt()
-        {
-            Console.WriteLine("Enter serial number of month, from season u've chosen: ");
-
-            int num = int.Parse(Console.ReadLine());
-
-            while (num < 1 || num > 12)
-            {
-                Console.WriteLine("Your number is not correct. You should to chose number from 1 to 12. Try again.");
-                num = int.Parse(Console.ReadLine());
-            }
-            return num;
+            string workDir = @"D:\Homework_Lesson_5";
+            Console.WriteLine(Directory.Exists(workDir)); 
+            
+            string notesDir = Path.Combine(workDir, "New directory 5.1");
+            
+            Directory.CreateDirectory(notesDir); 
+            // Создаем вложенную директорию
+            string noteText = Console.ReadLine(); //Ввод данных в блокнот через консоль
+            
+            string notePath = Path.Combine(notesDir, "EnterSomeText.txt");
+            
+            File.WriteAllText(notePath, noteText);
         }
     }
 }
