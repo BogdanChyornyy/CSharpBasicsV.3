430cedb1d5d5711fee3225004649a53bd88f5362
BogdanChyornyy <booggiman@mail.ru>
2022-04-28 12:14:26 +0300

    Выполнил домашнее задание к уроку 6.

--- CSharpBasicsV.3/Program.cs
+++ CSharpBasicsV.3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+public class Program
+{
+	public static void Main()
+	{
+		string start = "open";
+		while (start == "open")
+        {
+			ProcessKiller();
+			Console.WriteLine("Program process will be continued, do you agree?");
+			Console.WriteLine("yes/no");
+			string point = Console.ReadLine();
+			if (point == "no")
+            {
+				Console.WriteLine("Ending program...");
+				break;
+            }            
+        }
+				
+	}
+
+	public static void ProcessKiller()
+	{
+		Process chosen = Process.GetProcessById(Int32.Parse(ListOfProcesses()));
+		chosen.Kill();
+	}
+
+	public static string ListOfProcesses()
+	{
+		Process[] allprocesses = Process.GetProcesses();
+
+		foreach (var process in allprocesses)
+		{
+			// выводим id и имя процесса
+			Console.WriteLine("ID:" + process.Id + " Name: " + process.ProcessName);
+		}
+
+		Console.WriteLine("Enter processes ID to close...");
+		string id = Console.ReadLine();
+		return id;
+	}
+}
