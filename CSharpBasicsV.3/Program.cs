94719dbd3a565701dea877fbcf87a62fce62deb0
BogdanChyornyy <booggiman@mail.ru>
2022-04-28 12:22:42 +0300

    Ветка мастер обновлена

--- CSharpBasicsV.3/Program.cs
+++ CSharpBasicsV.3/Program.cs
@@ -7,18 +7,18 @@ public class Program
 	{
 		string start = "open";
 		while (start == "open")
-        {
+		{
 			ProcessKiller();
 			Console.WriteLine("Program process will be continued, do you agree?");
 			Console.WriteLine("yes/no");
 			string point = Console.ReadLine();
 			if (point == "no")
-            {
+			{
 				Console.WriteLine("Ending program...");
 				break;
-            }            
-        }
-				
+			}
+		}
+
 	}
 
 	public static void ProcessKiller()
@@ -41,37 +41,4 @@ public class Program
 		string id = Console.ReadLine();
 		return id;
 	}
-=======
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string path = @"D:\Homework_Lesson_5\New directory 5.3\Example.bin";
-
-            using (BinaryWriter BW = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
-            {                
-                BW.Write(NumEnter());       
-                BW.Close();      
-                Console.ReadKey();
-            }
-
-        static int NumEnter()
-            {
-                int num = Convert.ToInt32(Console.ReadLine());
-                while (num <= 0 || num > 255)
-                {
-                    Console.WriteLine("Uncorrect number!");
-                    num = Convert.ToByte(Console.ReadLine());
-                }                
-                return num;
-            }               
-        }
-    }
 }
