2ae769fb2b0a136d745c6e8fcb4610646900d0a3
BogdanChyornyy <booggiman@mail.ru>
2022-05-04 01:26:48 +0300

    Попытка 2

--- CSharpBasicsV.3/Program.cs
+++ CSharpBasicsV.3/Program.cs
@@ -1,44 +1,22 @@
 ﻿using System;
 using System.Diagnostics;
 
-public class Program
+namespace Homework
 {
-	public static void Main()
+
+	public class Program
 	{
-		string start = "open";
-		while (start == "open")
+		public static void Main(string[] args)
 		{
-			ProcessKiller();
-			Console.WriteLine("Program process will be continued, do you agree?");
-			Console.WriteLine("yes/no");
-			string point = Console.ReadLine();
-			if (point == "no")
+			string secret = "hello";
+			Console.WriteLine("Enter password: ");
+			string input = Console.ReadLine();
+			if (input == secret)
 			{
-				Console.WriteLine("Ending program...");
-				break;
+				Console.WriteLine("Welcome!");
 			}
+
+			Console.ReadKey(true);
 		}
-
-	}
-
-	public static void ProcessKiller()
-	{
-		Process chosen = Process.GetProcessById(Int32.Parse(ListOfProcesses()));
-		chosen.Kill();
-	}
-
-	public static string ListOfProcesses()
-	{
-		Process[] allprocesses = Process.GetProcesses();
-
-		foreach (var process in allprocesses)
-		{
-			// выводим id и имя процесса
-			Console.WriteLine("ID:" + process.Id + " Name: " + process.ProcessName);
-		}
-
-		Console.WriteLine("Enter processes ID to close...");
-		string id = Console.ReadLine();
-		return id;
 	}
 }
